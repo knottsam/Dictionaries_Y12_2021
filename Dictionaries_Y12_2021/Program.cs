bcf8852b23ae4753a402db5a8855dd42e86155b5
Sam Knott <SKnott@smchull.org>
2024-07-11 09:44:54 +0100

    Add project files.

---
+++ Dictionaries_Y12_2021/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionaries_Y12_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> exampleDictionary = 
+                new Dictionary<int, string>();
+
+
+            exampleDictionary.Add(1, "Maz");
+            exampleDictionary.Add(2, "Jack");
+            exampleDictionary.Add(3, "Szymon");
+            exampleDictionary.Add(4, "Maz");
+
+            foreach (KeyValuePair<int, string> pair in exampleDictionary)
+            {
+                Console.WriteLine($"The key {pair.Key}'s value is: {pair.Value}.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
